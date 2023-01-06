ec7f48d46cd691b756d1f8a84292b9e227299497
Emre Arapcic Uevak <EmreArapcicUevak@gmail.com>
2023-01-06 18:02:18 +0100

    Add key pad buttons to the gui

--- coffee-machine-simulator.m
+++ coffee-machine-simulator.m
@@ -24,6 +24,21 @@ coffeeMenu = uipanel('Parent', leftHalf, 'position', [0.025 0.05 0.95 .9]);
 % Set up all elements
 consoleOutput = uicontrol(rightHalf, 'Style', 'edit', 'units', 'normalized' ,"string", "plot title: (text)","position", [0.05 0.85 0.9 0.13], 'Max', 5, 'Min', 0, 'enable', 'off');
 
-
+% Set up KeyPad Buttons
+for i = 1:4
+	for j = 1:3
+		if (i != 4)
+			uicontrol(keypadFrame, 'Style', 'pushbutton', 'units', 'normalized' ,"string", num2str(3*(i-1) + j - 1),"position", [1/3*(j-1) 1-1/4*i 1/3 1/4], 'Max', 5, 'Min', 0, 'enable', 'off');
+		else
+			if (j == 2)	
+				uicontrol(keypadFrame, 'Style', 'pushbutton', 'units', 'normalized' ,"string", "9","position", [1/3 0 1/3 1/4], 'Max', 5, 'Min', 0, 'enable', 'off');
+			elseif (j == 1)
+				uicontrol(keypadFrame, 'Style', 'pushbutton', 'units', 'normalized' ,"string", "Yes","position", [0 0 1/3 1/4], 'Max', 5, 'Min', 0, 'enable', 'off');
+			else
+				uicontrol(keypadFrame, 'Style', 'pushbutton', 'units', 'normalized' ,"string", "No","position", [2/3 0 1/3 1/4], 'Max', 5, 'Min', 0, 'enable', 'off');
+			end
+		end
+	end
+end
 guidata(mainFrame, data);
 set(consoleOutput,'string', 'test');
