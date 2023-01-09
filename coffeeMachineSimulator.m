5d688f87dbff38914783e0ee82355e74998a6cc6
VedadSiljic <siljicvedad17@gmail.com>
2023-01-09 19:48:19 +0100

    Add interaction menu in the menubar

--- coffeeMachineSimulator.m
+++ coffeeMachineSimulator.m
@@ -10,6 +10,9 @@ function coffeeMachineSimulator()
   # Set up the GUI of the application
   setupGUI();
 
+  data = guidata(gcf());
+  #set(data.consoleOutput, "string", "vedad");
+
 endfunction
 
 
@@ -27,8 +30,8 @@ function setupGUI()
   data.coffeeNames{6} = "Hot Chocolate";
 
   # Set up all needed frames
-  mainFrame = figure('position', [0 0 800 600], 'Name', 'Coffe Machine', 'NumberTitle', 'off','color', primaryColor,'toolbar','none', 'menubar', 'none','resize', 'off');
-  titleFrame = uipanel('Parent', mainFrame, 'position', [0 .85 1 .15],'HighLightColor', primaryColor );
+  mainFrame = figure('position', [0 0 800 600], 'Name', 'Coffee Machine', 'NumberTitle', 'off','color', primaryColor,'toolbar','none', 'resize', 'off');
+  titleFrame = uipanel('Parent', mainFrame, 'position', [0 .85 1 .15],'HighLightColor', primaryColor);
 
   rightHalf = uipanel('Parent', mainFrame, 'position', [0.5 0 0.5 .85],'backgroundcolor', primaryColor, 'HighLightColor', primaryColor );
   keypadFrame = uipanel('Parent', rightHalf, 'position', [0.05 0.3 0.5 0.5],'HighLightColor', primaryColor );
@@ -38,23 +41,34 @@ function setupGUI()
   coffeeMenu = uipanel('Parent', leftHalf, 'position', [0.025 0.05 0.95 .9], 'backgroundcolor', primaryColor,'HighLightColor', primaryColor );
 
   # Set up all elements
-  data.consoleOutput = uicontrol(rightHalf, 'Style', 'edit', 'units', 'normalized' ,"string", "plot title: (text)","position", [0.05 0.85 0.9 0.13], 'Max', 5, 'Min', 0, 'enable', 'off','backgroundcolor', '#000000');
+  data.consoleOutput = uicontrol(rightHalf, 'Style', 'edit', 'units', 'normalized' ,"string", "Do you want coffee bitch???","position", [0.05 0.85 0.9 0.13], 'Max', 5, 'Min', 0, 'enable', 'off','backgroundcolor', '#000000');
 
   # Set up title
   uicontrol('parent', titleFrame, 'Style', 'text', 'units', 'normalized' ,"string", "IUS COFFEE","position", [0 0 1 1], 'backgroundcolor', primaryColor, 'fontsize', 18);
+
+  # Set up menubar
+  interactions = uimenu("Text", "Interactions");
+  interactionsItem1 = uimenu(interactions, "Text", "Insert 5KM", 'callback', { @interactionPressed, "5KM" });
+  interactionsItem2 = uimenu(interactions, "Text", "Insert 2KM", 'callback', { @interactionPressed, "2KM" });
+  interactionsItem3 = uimenu(interactions, "Text", "Insert 1KM", 'callback', { @interactionPressed, "1KM" });
+  interactionsItem4 = uimenu(interactions, "Text", "Insert 50F", 'callback', { @interactionPressed, "50F" });
+  interactionsItem5 = uimenu(interactions, "Text", "Insert 20F", 'callback', { @interactionPressed, "20F" });
+  interactionsItem6 = uimenu(interactions, "Text", "Insert 10F", 'callback', { @interactionPressed, "10F" });
+  interactionsItem7 = uimenu(interactions, "Text", "Insert 5F", 'callback', { @interactionPressed, "5F" });
+  interactionsItem8 = uimenu(interactions, "Text", "Take the coffee", 'callback', { @interactionPressed, "ttc" });
 
   # Set up KeyPad Buttons
   for i = 1:4
 	  for j = 1:3
 		  if (i != 4)
-			  uicontrol(keypadFrame, 'Style', 'pushbutton', 'units', 'normalized' ,"string", num2str(3*(i-1) + j - 1),"position", [1/3*(j-1) 1-1/4*i 1/3 1/4], 'Max', 5, 'Min', 0, 'enable', 'on', 'fontsize', 14, 'backgroundcolor', secondaryColor);
+			  uicontrol(keypadFrame, 'Style', 'pushbutton', 'units', 'normalized' ,"string", num2str(3*(i-1) + j - 1), 'callback', { @buttonPressed, num2str(3*(i-1) + j - 1) }, "position", [1/3*(j-1) 1-1/4*i 1/3 1/4], 'Max', 5, 'Min', 0, 'enable', 'on', 'fontsize', 14, 'backgroundcolor', secondaryColor);
 		  else
 			  if (j == 2)
-				  uicontrol(keypadFrame, 'Style', 'pushbutton', 'units', 'normalized' ,"string", "9","position", [1/3 0 1/3 1/4], 'Max', 5, 'Min', 0, 'enable', 'on', 'fontsize', 14,'backgroundcolor', secondaryColor);
+				  uicontrol(keypadFrame, 'Style', 'pushbutton', 'units', 'normalized' ,"string", "9", 'callback', { @buttonPressed, "9" }, "position", [1/3 0 1/3 1/4], 'Max', 5, 'Min', 0, 'enable', 'on', 'fontsize', 14,'backgroundcolor', secondaryColor);
 			  elseif (j == 1)
-				  uicontrol(keypadFrame, 'Style', 'pushbutton', 'units', 'normalized' ,"string", "Yes","position", [0 0 1/3 1/4], 'Max', 5, 'Min', 0, 'enable', 'on', 'fontsize', 14,'backgroundcolor', secondaryColor);
+				  uicontrol(keypadFrame, 'Style', 'pushbutton', 'units', 'normalized' ,"string", "Yes", 'callback', { @buttonPressed, "yes" }, "position", [0 0 1/3 1/4], 'Max', 5, 'Min', 0, 'enable', 'on', 'fontsize', 14,'backgroundcolor', secondaryColor);
 			  else
-				  uicontrol(keypadFrame, 'Style', 'pushbutton', 'units', 'normalized' ,"string", "No","position", [2/3 0 1/3 1/4], 'Max', 5, 'Min', 0, 'enable', 'on', 'fontsize', 14,'backgroundcolor', secondaryColor);
+				  uicontrol(keypadFrame, 'Style', 'pushbutton', 'units', 'normalized' ,"string", "No", 'callback', { @buttonPressed, "no" }, "position", [2/3 0 1/3 1/4], 'Max', 5, 'Min', 0, 'enable', 'on', 'fontsize', 14,'backgroundcolor', secondaryColor);
 			  end
 		  end
 	  end
@@ -76,13 +90,16 @@ function setupGUI()
 
 endfunction
 
+function buttonPressed(hObject, eventdata, value)
 
+  data = guidata(gcf());
+  set(data.consoleOutput, "string", value);
 
+endfunction
 
+function interactionPressed(hObject, eventdata, value)
 
+  data = guidata(gcf());
+  set(data.consoleOutput, "string", value);
 
-
-
-
-
-
+endfunction
